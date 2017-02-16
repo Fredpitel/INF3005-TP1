de2611477ac2b73577929b852a4cc83fe075c9ee
Fredpitel <fredpitel@gmail.com>
2017-02-16 17:10:03 -0500

    GET / et /recherche complet

---
+++ database.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+
+class Database:
+    def __init__(self):
+        self.connection = None
+
+    def get_connection(self):
+        if self.connection is None:
+            self.connection = sqlite3.connect("C:\\INF3005\\TP1\\db\\article.db")
+        return self.connection
+
+    def disconnect(self):
+        if self.connection is not None:
+            self.connection.close()
+
+    def get_articles(self, date):
+        cursor = self.get_connection().cursor()
+        cursor.execute("SELECT * FROM article WHERE date_publication = '{}' LIMIT 5;".format(date))
+        return cursor.fetchall()
+
+    def rechercher_articles(self, recherche):
+        cursor = self.get_connection().cursor()
+        cursor.execute("SELECT * FROM article "
+                       "WHERE titre LIKE '%{}%' OR paragraphe LIKE '%{}%';".format(recherche, recherche))
+        return cursor.fetchall()
---
+++ index.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+from flask import g
+from flask import request
+from flask import redirect
+from flask import url_for
+from database import Database
+import datetime
+
+app = Flask(__name__, static_url_path="", static_folder="static")
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        g._database = Database()
+    return g._database
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.disconnect()
+
+
+@app.route('/')
+def page_accueil():
+    articles = get_db().get_articles(datetime.date.today().isoformat())
+    return render_template('acceuil.html', articles=articles)
+
+
+@app.route('/rechercher', methods=['GET'])
+def rechercher():
+    articles = get_db().rechercher_articles(request.args['recherche'])
+    return render_template('recherche.html', articles=articles)
